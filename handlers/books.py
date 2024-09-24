8b90a8bdcbf1869323c64db8993d8e26619c14bd
Vadosdavos <bezbvap@gmail.com>
2024-09-24 21:40:38 +0200

    feat: add formating

--- handlers/books.py
+++ handlers/books.py
@@ -3,16 +3,21 @@ from aiogram.types import Message
 
 from middleware.chat_action import ChatActionMiddleware
 from utils.ai import generate_recommendation
+from aiogram.utils.formatting import Text, Bold, as_list, as_marked_list
 
 router = Router()
 router.message.middleware(ChatActionMiddleware())
 
 @router.message(F.text, flags={'long_operation': 'typing'})
-async def cmd_book_recommendation(message: Message):
+async def get_book_recommendations(message: Message):
     prompt = message.text
     answer = await message.answer("Подбираю рекомендации...")
     res = await generate_recommendation(prompt)
+    books_list = []
+    for book in res:
+        books_list.append(Text(Bold(book['title']), ' - ', f'{book['author']}. ', book['description'] ))
+    content = as_list(f'Если вам понравилась {prompt}, возможно, вам будут интересны следующие книги:', as_marked_list(*books_list, marker='📚'), "Надеюсь, эти рекомендации помогут вам найти новые интересные книги, которые вам понравятся!", sep="\n\n")
     if res:
-        return await answer.edit_text(res)
+        return await answer.edit_text(**content.as_kwargs())
     else: 
         return await answer.edit_text("Error")
--- utils/ai.py
+++ utils/ai.py
@@ -1,21 +1,12 @@
 from utils.config_reader import config
 from mistralai import Mistral
 import logging
+import json
 
 api_key = config.api_key.get_secret_value()
 model = "pixtral-12b-2409"
 
 client = Mistral(api_key=api_key)
-
-chat_response = client.chat.complete(
-    model= model,
-    messages = [
-        {
-            "role": "user",
-            "content": "What is the best French cheese?",
-        },
-    ]
-)
 
 async def generate_recommendation(prompt) -> str:
     try:
@@ -24,12 +15,14 @@ async def generate_recommendation(prompt) -> str:
             messages = [
                 {
                     "role": "user",
-                    "content": f'Посоветуй книгу похожую на книгу: {prompt}',
+                    "content": f'Представь, что ты опытный литературный критик, который прочитал почти все существующие книги. Посоветуй несколько книг, похожих на книгу: {prompt}.' + "Верни рекомендации в виде короткого JSON объекта вот с такой структурой: 'books': [{'title': string, 'author': string, 'description': string }]",
                 },
-            ]
+            ],
+            response_format = {
+                "type": "json_object",
+            }
         )
-        print(response)
-        print(f'Total tokens: {response.usage.total_tokens}')
-        return response.choices[0].message.content
+        books = json.loads(response.choices[0].message.content)['books']
+        return books
     except Exception as e:
         logging.error(e)
